203b9ff4fc0fc36523ad0ea889d7537c07843fec
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-11-18 02:49:40 -0500

    test

--- scraper/scrapeRecommendations.py
+++ scraper/scrapeRecommendations.py
@@ -2,12 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from requests_html import HTMLSession
 
+session = HTMLSession()
+
+product_list = []
+
+for i in range(6):
+  r = session.get(f'https://www.hollisterco.com/shop/us/mens-tops?filtered=true&rows=90&start={i * 90}')
+
+  product_list.extend([i for i in list(r.html.links) if i.startswith("/shop/us/p/")])
+  
 
 def get_rec_images(url):
     driver = webdriver.Chrome()
     driver.get(url)
-    #wait for the div id called "stylistics-widget" to load
+
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "stylitics-widget")))
     temp = driver.find_elements(By.CLASS_NAME, "stylitics-card")
     links = []
@@ -15,3 +25,6 @@ def get_rec_images(url):
         xpath_link = f"/html/body/main/section[1]/div[2]/section/div/div/div/div[1]/div/div[{i}]/div/div[3]/div[1]"
         links.append((driver.find_element('xpath', xpath_link)).get_attribute("data-src"))        
     return links
+
+for i in product_list:
+   print(get_rec_images('https://www.hollisterco.com/' + i))
--- scraper/test.py
+++ scraper/test.py
@@ -1,29 +1,27 @@
-# import openai
+import openai
 
-# # client = OpenAI()s
+response = openai.chat.completions.create(
+  model="gpt-4-vision-preview",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "Describe every (or the only) clothing item in these image in detail, be sure to pay attention to little things in the clothing items. Do not talk about the relative positioning of the items, meaning talk about each of the items out of context of the image - as if they were a seperate image. ONLY return the descriptions of the items seperate by a new line."},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://img.hollisterco.com/is/image/anf/KIC_332-3037-1777-235_prod2.jpg?policy=product-large",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
 
-# response = openai.chat.completions.create(
-#   model="gpt-4-vision-preview",
-#   messages=[
-#     {
-#       "role": "user",
-#       "content": [
-#         {"type": "text", "text": "Describe every (or the only) clothing item in this image in detail, be sure to pay attention to little things in the clothing items. Do not talk about the relative positioning of the items, meaning talk about each of the items out of context of the image - as if they were a seperate image. ONLY return the descriptions of the items seperate by a new line."},
-#         {
-#           "type": "image_url",
-#           "image_url": {
-#             "url": "https://cdn.discordapp.com/attachments/588057941714927625/1175276045558108262/IMG20231117222716.jpg?ex=656aa41c&is=65582f1c&hm=9516bb3c9e4a521e54b4fefc3d2a7f5eb1ada2129952d8baf6a350268cacfa08&",
-#           },
-#         },
-#       ],
-#     }
-#   ],
-#   max_tokens=300,
-# )
-
-# print(response.choices[0])
+print(response)
 
 
-import requests
+# import requests
 
-print(requests.get("https://www.hollisterco.com/"))
+# print(requests.get("https://www.hollisterco.com/"))
