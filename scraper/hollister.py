a49c58e680966b142479de5f98c595a3951b85ce
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-11-17 23:26:47 -0500

    selenium test

--- scraper/hollister.py
+++ scraper/hollister.py
@@ -1,1 +1,10 @@
-import selenium
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path='./chromedriver.exe')
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('https://www.hollisterco.com/')
